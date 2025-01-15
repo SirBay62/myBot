20501dc8bc74dc189b95d905786b995c9e46050f
SirBay62 <sir.bay@yandex.ru>
2025-01-15 17:02:31 +0300

    Добавлен бот с токеном

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import asyncio
 from aiogram import Bot, Dispatcher
 
-bot = Bot(token='')
+bot = Bot(token='8153480092:AAFjypyPyk76mAowNHGTIIN4gv3VKOmbBNE')
 dp = Dispatcher(bot)
 
 async def main():
     await db.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
