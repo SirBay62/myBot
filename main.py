3dbff301807ff3dfb550d0c5fb3ce40746214704
SirBay62 <sir.bay@yandex.ru>
2025-01-16 14:37:25 +0300

    Бот работает ожидаемым образом

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
+from aiogram.filters import Command, CommandStart
 
 bot = Bot(token='8153480092:AAFjypyPyk76mAowNHGTIIN4gv3VKOmbBNE')
 dp = Dispatcher()
 
-
-@dp.message()
-async def cmd_start(message: Message):
-    await message.answer('Привет!')
-    await message.reply('Как дела?')
-#
-# @dp.message
-# async def cmd_name(message: Message):
-#     await message.reply("Привет! Я бот.")
-
-# @dp.message_handler(commands=['start'])
-# async def on_start(message: types.Message):
+# @dp.message()
+# async def cmd_start(message: Message):
 #     await message.answer('Привет!')
-#     pass
+#     await message.reply('Как дела?')
+
+@dp.message(CommandStart())
+async def cmd_name(message: Message):
+    await message.reply("Привет! Давай поработаем.")
+
+# @dp.message(Command(BotCommand(command="привет")))
+@dp.message(Command("имя"))
+@dp.message(F.text.casefold() == "имя")
+async def on_name(message: Message):
+    await message.answer('Меня зовут AlexBot')
+
+@dp.message(F.text)
+async def on_name(message: Message):
+    await message.answer('Неизвестная команда')
+
+# @dp.message()
+# async def cmd_start(message: Message):
+#     await message.answer('Неизвестная команда!')
 
 async def main():
     await dp.start_polling(bot)
