6b0ea7641f083e87d0a7ad7a61a7017402c512ba
SirBay62 <sir.bay@yandex.ru>
2025-01-16 12:58:45 +0300

    Бот снова работает

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ from aiogram.types import Message
 bot = Bot(token='8153480092:AAFjypyPyk76mAowNHGTIIN4gv3VKOmbBNE')
 dp = Dispatcher()
 
+
 @dp.message()
 async def cmd_start(message: Message):
     await message.answer('Привет!')
     await message.reply('Как дела?')
+#
+# @dp.message
+# async def cmd_name(message: Message):
+#     await message.reply("Привет! Я бот.")
+
+# @dp.message_handler(commands=['start'])
+# async def on_start(message: types.Message):
+#     await message.answer('Привет!')
+#     pass
 
 async def main():
     await dp.start_polling(bot)
